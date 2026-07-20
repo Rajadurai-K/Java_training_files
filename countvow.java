a291c8734bbe15084fccad637ffb9c44382cf820
Rajadurai K <irajavduraik@gmail.com>
2026-07-20 10:37:33 +0530

    java files are added

--- countvow.java
+++ countvow.java
@@ -1,18 +1,46 @@
+import java.util.Scanner;
 public class countvow {
     public static void main(String[] args)
     {
-    String str = "Java programming";
-    int count = 0;
-    str= str.toLowerCase();
-    for(int i =0;i<str.length();i++)
-    {
-        char c = str.charAt(i);
     
-    if(c=='a'||c=='e'||c=='i'||c=='o'||c=='u')
+    Scanner sc = new Scanner(System.in);
+    String str = sc.nextLine();
+    System.out.println("Enter the String values"); 
+     int a =0;
+    int e =0;
+    int i =0;
+    int o =0;
+    int u =0;
+    str= str.toLowerCase();
+    for(int ii =0;ii<str.length();ii++)
     {
-        count++;
+        char c = str.charAt(ii);
+   
+    if(c=='a')
+    {
+        a++;
+    }
+    if(c=='e')
+    {
+        e++;
+    }
+    if(c=='i')
+    {
+        i++;
+    }
+    if(c=='o')
+    {
+        o++;
+    }
+    if(c=='u')
+    {
+        u++;
     }
     }
-    System.out.println("Number of vowels " +count);
+    System.out.println("Number of a " +a);
+    System.out.println("Number of e " +e);
+    System.out.println("Number of i " +i);
+    System.out.println("Number of o " +o);
+    System.out.println("Number of u " +u);
     }
 }
